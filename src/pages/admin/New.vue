48bdb3f619c30abbf9f34f06f83562cd9c9f5629
Fong Mok <fong.mok@me.com>
2019-10-03 14:05:06 +0200

    Form validation

--- src/pages/admin/New.vue
+++ src/pages/admin/New.vue
@@ -1,8 +1,99 @@
 <!-- ./src/pages/admin/New.vue -->
 <template>
-  <div>
-    <div class="title">
-      <h1>This is Admin/New</h1>
+  <form @submit.prevent="saveProduct">
+    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+      <div class="form-group">
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="Name"
+          v-model="model.name"
+          v-validate="'required'"
+          name="name"
+          :class="{'form-control': true, 'error': errors.has('name') }" />
+        <span class="small text-danger" v-show="errors.has('name')">Name is required</span>
+      </div>
+      <div class="form-group">
+        <label>Price</label>
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Price"
+          v-model="model.price"
+          v-validate="'required'"
+          name="price"
+          :class="{'form-control': true, 'error': errors.has('price') }" />
+        <span class="small text-danger" v-show="errors.has('price')">Price is required</span>
+      </div>
+      <div class="form-group">
+
+        <label>Manufacturer</label>
+        <select
+          type="text"
+          class="form-control"
+          v-model="model.manufacturer"
+          v-validate="'required'"
+          name="manufacturer"
+          :class="{'form-control': true, 'error': errors.has('manufacturer') }">
+          <template v-for="manufacturer in manufacturers">
+            <option :value="manufacturer._id" :selected="manufacturer._id == (model.manufacturer && model.manufacturer._id)" add v-bind:key="manufacturer.id">{{manufacturer.name}}</option>
+          </template>
+        </select>
+        <span class="small text-danger" v-show="errors.has('manufacturer')">Manufacturer is required</span>
+      </div>
     </div>
-  </div>
+
+    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+      <div class="form-group">
+        <label>Image</label>
+        <input
+          type="text"
+          lass="form-control"
+          placeholder="Image"
+          v-model="model.image"
+          v-validate="'required|url'"
+          name="image"
+          :class="{'form-control': true, 'error': errors.has('image') }" />
+        <span class="small text-danger" v-show="errors.has('image')">Image is required and must be a valid URL</span>
+      </div>
+      <div class="form-group">
+        <label>Description</label>
+        <textarea
+          type="number"
+          class="form-control"
+          placeholder="Description"
+          rows="5"
+          v-model="model.description"
+          v-validate="'required'"
+          name="description"
+          :class="{'form-control': true, 'error': errors.has('description') }"></textarea>
+        <span class="small text-danger" v-show="errors.has('description')">Description is required</span>
+      </div>
+      <div class="form-group new-button">
+        <button class="button">
+          <i class="fa fa-pencil"></i>
+          <span v-if="isEditing">Update Product</span>
+          <span v-else>Add Product</span>
+        </button>
+      </div>
+    </div>
+  </form>
 </template>
+
+<script>
+export default {
+  props: ['isEditing'],
+  data: function data () {
+    return {
+      model: { },
+      manufacturers: [{ _id: '0', name: 'Samsung' }, { _id: '1', name: 'Apple' }]
+
+    }
+  },
+  methods: {
+    saveProduct: function saveProduct () {
+      this.$validator.validate()
+    }
+  }
+}
+</script>
